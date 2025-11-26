b52bef62d0bca6e6d282a3307ca2e1bfba40d6db
MaShuku <155946572+Snethemba123@users.noreply.github.com>
2025-11-26 23:54:53 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,43 +1,43 @@
-document.addEventListener("DOMContentLoaded", function () {
+//document.addEventListener("DOMContentLoaded", function () {
 
   // Mobile menu toggle
-  (function () {
-    const toggle = document.querySelector('.menu-toggle');
-    const nav = document.getElementById('main-nav');
+  //(function () {
+  //  const toggle = document.querySelector('.menu-toggle');
+   // const nav = document.getElementById('main-nav');
 
-    if (!toggle || !nav) return;
+   // if (!toggle || !nav) return;
 
-    toggle.addEventListener('click', function () {
-      nav.classList.toggle('open');
-    });
+    //toggle.addEventListener('click', function () {
+     // nav.classList.toggle('open');
+   // });
 
-    // Close menu when clicking a link
-    nav.querySelectorAll("a").forEach(link => {
-      link.addEventListener("click", () => {
-        nav.classList.remove("open");
-      });
-    });
-  })();
+    //// Close menu when clicking a link
+    //nav.querySelectorAll("a").forEach(link => {
+     // link.addEventListener("click", () => {
+      //  nav.classList.remove("open");
+      //});
+   // });
+ // })();
 
  // Mobile dropdown fix 
-const dropdowns = document.querySelectorAll(".dropdown");
+//const dropdowns = document.querySelectorAll(".dropdown");
 
-dropdowns.forEach(drop => {
-    const button = drop.querySelector(".dropbtn");
+//dropdowns.forEach(drop => {
+ //   const button = drop.querySelector(".dropbtn");
 
-    button.addEventListener("click", function (e) {
+ //   button.addEventListener("click", function (e) {
 
         // Only apply on mobile
-        if (window.innerWidth < 769) {
-            e.preventDefault();
+     //   if (window.innerWidth < 769) {
+          //  e.preventDefault();
 
             // Close other dropdowns
-            dropdowns.forEach(d => {
-                if (d !== drop) d.classList.remove("open-dropdown");
-            });
+           // dropdowns.forEach(d => {
+          //      if (d !== drop) d.classList.remove("open-dropdown");
+          //  });
 
             // Toggle current dropdown
-            drop.classList.toggle("open-dropdown");
-        }
-    });
-});
+          // drop.classList.toggle("open-dropdown");
+     //   }
+   // });
+//});
